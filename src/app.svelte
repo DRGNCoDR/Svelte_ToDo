c715d126423615f9bf0dd26406f4467c81258d1e
DragonCoder <rayfradella@yahoo.com>
2022-08-21 11:35:56 -0400

    Display list of todos with delete capability. Add some border boxes for clarity.

--- src/app.svelte
+++ src/app.svelte
@@ -4,36 +4,30 @@ $: todos = []
 const Add = (todo) => {
 todo.id = Date.now()
 todos.push(todo)
-console.log(todos)
+todos = todos
 }
 
 const Remove = (id) => {
 todos = todos.filter(todos => todos.id != id)
-console.log(todos)
 }
+
+// const ToggleComplete = (todo) => {
+//     //document.getElementById("todo-" + {id}).classList.toggle("complete")
+// }
 </script>
 
 <h1>
 Todos
 </h1>
-
 <div>
-    {#each todos as todo}
-    <div>
-        <button on:click={Remove(todo.id)}>X</button>
-    </div>
-    {/each}
-</div>
-<div>
-    <form on:submit|preventDefault = {Add({...todos})}>
+    <!--Component 1: Add-->
+    <form
+    on:submit|preventDefault = {Add({...todos})}>
         <div>
             <div>
                 <input
                     bind:value={todos.title}
                     placeholder="Title"/>
-                <input type="checkbox"
-                    bind:checked={todos.complete}
-                    placeholder="Done"/>
             </div>
             <div>
                 <textarea
@@ -43,4 +37,48 @@ Todos
             </div>
         </div>
     </form>
+
+    <!--Component 2: List-->
+    <div class="border-black">
+        {#each todos as todo}
+            <div
+                class="border-red"
+            >
+                <h4 class="title-{todo.id}">
+                    {todo.title}
+                </h4>
+
+                <p>
+                    {todo.description}
+                </p>
+
+                <input
+                class="check-{todo.id} "
+                type="checkbox"
+                    bind:checked={todo.checked}
+                    placeholder="Done"/>
+
+                <div>
+                    <button on:click={Remove(todo.id)}>
+                        X
+                    </button>
+                </div>
+            </div>
+        {/each}
+    </div>
 </div>
+
+<style>
+    .border-red{
+        border: 2px solid red;
+    }
+    .border-green{
+        border: 2px solid green;
+    }
+    .border-black{
+        border: 2px solid black;
+    }
+    .complete{
+        text-decoration: line-through;
+    }
+</style>
